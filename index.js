1f938173820c4db050d277553a7f625e7fc90a9b
T3tje <tilman2013@gmail.com>
2022-01-11 20:09:58 +0100

    uibuild

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response,next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -46,9 +46,14 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
+  note.save()
+  .then(savedNote => {      
+    return savedNote.toJSON()    
+  })    
+  .then(savedAndFormattedNote => {      
+    response.json(savedAndFormattedNote)    
   }) 
+  .catch(error => next(error)) 
 })
 
 app.get('/api/notes/:id', (request, response, next) => {  Note.findById(request.params.id)
@@ -95,7 +100,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })  
+    }
 
   next(error)
 }
