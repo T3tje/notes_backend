f4326f2c41e61fc519ad2dd75bb0e589f05c19ca
T3tje <tilman2013@gmail.com>
2022-01-18 15:20:46 +0100

    tonotebook

--- models/note.js
+++ models/note.js
@@ -2,35 +2,35 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result  => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
-    })
+  .then(()  => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
-    const noteSchema = new mongoose.Schema({
-      content: {    
-        type: String,
-        minLength: 5,
-        required: true
-      },  
-      date: {
-        type: Date,
-        required: true
-      },
-      important: Boolean
-      })
-      
-      noteSchema.set('toJSON', {
-        transform: (document, returnedObject) => {
-          returnedObject.id = returnedObject._id.toString()
-          delete returnedObject._id
-          delete returnedObject.__v
-        }
-      })
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean
+})
 
-      module.exports = mongoose.model('Note', noteSchema)
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
