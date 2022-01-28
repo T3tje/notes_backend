0d5d7960980c9abca05df756f3ca289ee7ee10d5
T3tje <tilman2013@gmail.com>
2022-01-28 15:26:57 +0100

    change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
@@ -10,7 +11,7 @@ const mongoose = require('mongoose')
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-  .then(() => {
+  .then(()  => {
     logger.info('connected to MongoDB')
   })
   .catch((error) => {
